17e1fff06da792e1ecaedfaa2aa27738893f07bb
Patryck <lecter933@gmail.com>
2023-11-21 06:17:30 +0100

    modified textfields added database

--- src/base_de_datos/u411959762_python_bdd.sql
+++ src/base_de_datos/u411959762_python_bdd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-11-2023 a las 23:13:25
+-- Tiempo de generación: 21-11-2023 a las 06:16:28
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.0.28
 
@@ -31,8 +31,80 @@ CREATE TABLE `tareas` (
   `id` int(11) NOT NULL,
   `Tarea` text NOT NULL,
   `Comentarios` text NOT NULL,
-  `Activos` date NOT NULL
+  `Activos` date NOT NULL,
+  `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `tareas`
+--
+
+INSERT INTO `tareas` (`id`, `Tarea`, `Comentarios`, `Activos`, `user_id`) VALUES
+(6, 'sadasd', 'sadasd', '0000-00-00', 3),
+(7, 'qweqweqw', 'qwewqe', '0000-00-00', 3),
+(9, 'dasdsad', 'qeweqew', '0000-00-00', 8),
+(11, '31231231', 'wqeqwewq', '0000-00-00', 8),
+(12, 'sadasd', 'sadasd', '2023-11-07', 3),
+(13, 'asdasd', 'sadasd', '2023-11-07', 8);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `username`, `password`) VALUES
+(1, 'qweqwe', 'dasdasdas'),
+(2, 'asdasd', 'weqweqw'),
+(3, 'lecter', 'abc123'),
+(4, 'poppp', 'adasd'),
+(5, 'xasxasx', 'ssss'),
+(6, 'juuu', 'asdsad'),
+(7, '12321', 'wqewqa'),
+(8, 'test', 'abc123');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `tareas`
+--
+ALTER TABLE `tareas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `tareas`
+--
+ALTER TABLE `tareas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
